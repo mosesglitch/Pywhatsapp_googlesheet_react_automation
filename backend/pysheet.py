7b8fe9aa5eca550036e0bcf21ef4472d70143fb7
mosesglitch <mosesglitch>
2023-01-05 02:28:41 +0300

    cp contacts

--- backend/pysheet.py
+++ backend/pysheet.py
@@ -111,6 +111,12 @@ def send_broadcast():
         else:
             print("{} has shared".format(j))
 
-
+names = {"Susan":"+255656389585",
+         "Moses" : "+255655509377",
+         "Richard":"+255623984029",  
+         "Happiness":"+255621346336",
+         "Present":"0"}
+test_name = {"susan":"+255656389585",
+             "conso":"+254712700283"}
 if __name__  == "__main__" :
     send_broadcast()
